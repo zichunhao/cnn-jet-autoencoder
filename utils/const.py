f15bbc8147d1b1ae8d4e16c7cd9313ba90a03f9d
Zichun Hao <zichunh2000@gmail.com>
2022-08-12 21:09:16 +0800

    Useful constants

--- utils/const.py
+++ utils/const.py
@@ -2,3 +2,5 @@ import torch
 
 DEFAULT_DTYPE = torch.float
 DEFAULT_DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+LOSS_BLOW_UP_THRESHOLD = 1e32
+MODES = ('train', 'valid', 'test')
